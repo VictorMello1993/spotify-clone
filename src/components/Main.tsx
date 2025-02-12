08cdf28169d212d6076f722a430f845b4714ea65
Victor Mello <victorsmello93@gmail.com>
2025-02-12 13:15:28 -0300

    Refatoração da lista de músicas e artistas populares

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,8 +3,8 @@ import ItemList from "./ItemList";
 export default function Main() {
 	return (
 		<div className="main">
-			<ItemList/>
-			<ItemList/>										
+			<ItemList title="Artistas" items={5} />
+			<ItemList title="Músicas" items={10} />
 		</div>
 	);
 }
